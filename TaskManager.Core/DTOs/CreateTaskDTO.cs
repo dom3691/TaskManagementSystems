1fd31ba4d8c55e80a5bff7aecb1b5b56fbcb1284
Dominic James <30402312+dom3691@users.noreply.github.com>
2023-09-30 10:19:49 +0100

    Implementations done, Cleaning Code

--- TaskManager.Core/DTOs/CreateTaskDTO.cs
+++ TaskManager.Core/DTOs/CreateTaskDTO.cs
@@ -4,14 +4,10 @@ namespace TaskManager.Core.DTO
 {
     public class CreateTaskDTO
     {
-        [Required]
-        public string TaskName { get; set; } = string.Empty;
-        [Required]
-        public string TaskDescription { get; set; } = string.Empty;
-        [Required]
-        public int AllottedTimeInDays { get; set; }
-        [Required]
-        public int ElapsedTimeInDays { get; set; }
+        [Required]  public string TaskName { get; set; } = string.Empty;
+        [Required]  public string TaskDescription { get; set; } = string.Empty;
+        [Required]  public int AllottedTimeInDays { get; set; }
+        [Required]  public int ElapsedTimeInDays { get; set; }
         public bool TaskStatus { get; set; }
     }
 }
