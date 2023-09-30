a8f99e6f9a32f527a880d6c06b0cea8965045485
Dominic James <30402312+dom3691@users.noreply.github.com>
2023-09-30 10:20:57 +0100

    Cleaned ResponseDto

--- TaskManager.Core/DTOs/ResponseDTO.cs
+++ TaskManager.Core/DTOs/ResponseDTO.cs
@@ -12,11 +12,22 @@ namespace TaskManager.Core.DTO
 
         public static ResponseDTO<T> Fail(string errorMessage, int statusCode = (int)HttpStatusCode.NotFound)
         {
-            return new ResponseDTO<T> { Status = false, Message = errorMessage, StatusCode = statusCode };
+            return new ResponseDTO<T>
+            {
+                Status = false,
+                Message = errorMessage,
+                StatusCode = statusCode
+            };
         }
         public static ResponseDTO<T> Success(string successMessage, T data, int statusCode = (int)HttpStatusCode.OK)
         {
-            return new ResponseDTO<T> { Status = true, Message = successMessage, Data = data, StatusCode = statusCode };
+            return new ResponseDTO<T>
+            {
+                Status = true,
+                Message = successMessage,
+                Data = data,
+                StatusCode = statusCode
+            };
         }
         public override string ToString() => JsonConvert.SerializeObject(this);
     }
